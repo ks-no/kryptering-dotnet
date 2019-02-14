6ebc31fe3d030a4b2bbc9bc776040a24d62cf68f
Sondre Eikanger Kvalø <zapodot@gmail.com>
2019-02-14 13:29:29 +0100

    Rename Test class

--- KS.Fiks.Crypto.Tests/OpenSslCryptoServiceTest.cs
+++ KS.Fiks.Crypto.Tests/OpenSslCryptoServiceTest.cs
@@ -6,19 +6,19 @@ using Xunit;
 
 namespace KS.Fiks.Crypto.Tests
 {
-    public class OpenSslCryptoService
+    public class OpenSslCryptoServiceTest
     {
         [Fact]
         public void CreateService()
         {
-            var openSslCryptoService = Crypto.OpenSslCryptoService.Factory.Create();
+            var openSslCryptoService = OpenSslCryptoService.Factory.Create();
             openSslCryptoService.Should().BeOfType<Crypto.OpenSslCryptoService>();
         }
 
         [Fact]
         public void Encrypt()
         {
-            var openSslCryptoService = Crypto.OpenSslCryptoService.Factory.Create();
+            var openSslCryptoService = OpenSslCryptoService.Factory.Create();
             var mockStream = new Mock<Stream>();
             openSslCryptoService.Invoking(s => s.Encrypt(mockStream.Object))
                 .Should().Throw<NotImplementedException>();
@@ -28,7 +28,7 @@ namespace KS.Fiks.Crypto.Tests
         [Fact]
         public void Decrypt()
         {
-            var openSslCryptoService = Crypto.OpenSslCryptoService.Factory.Create();
+            var openSslCryptoService = OpenSslCryptoService.Factory.Create();
             var mockStream = new Mock<Stream>();
             openSslCryptoService.Invoking(s => s.Decrypt(mockStream.Object))
                 .Should().Throw<NotImplementedException>();
