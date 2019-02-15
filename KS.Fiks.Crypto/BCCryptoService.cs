6da9621c2bd34866c852027566fab5ae695b843c
Sondre Eikanger Kvalø <zapodot@gmail.com>
2019-02-15 14:50:52 +0100

    Add ruleset for analysis

--- KS.Fiks.Crypto/BCCryptoService.cs
+++ KS.Fiks.Crypto/BCCryptoService.cs
@@ -7,36 +7,16 @@ namespace KS.Fiks.Crypto
 {
     public class BCCryptoService : ICryptoService
     {
-        public static class Factory
-        {
-            public static BCCryptoService Create(X509Certificate certificate)
-            {
-                if (certificate == null)
-                {
-                    throw new ArgumentNullException(nameof(certificate));
-                }
-
-                return new BCCryptoService(certificate);
-            }
-        }
-
-        private readonly X509Certificate _certificate;
+        private readonly X509Certificate certificate;
 
         public BCCryptoService(X509Certificate certificate)
         {
-            _certificate = certificate;
+            this.certificate = certificate;
         }
 
         public Stream Decrypt(Stream encryptedStream)
         {
             throw new NotImplementedException();
-        }
-
-        public Stream CreateEncryptionStream()
-        {
-            var cmsEnvelopedDataStreamGenerator = new CmsEnvelopedDataStreamGenerator();
-            cmsEnvelopedDataStreamGenerator.AddKeyTransRecipient(_certificate);
-            return new EncodedStream(new MemoryStream(), cmsEnvelopedDataStreamGenerator);
         }
 
         public Stream Encrypt(Stream unEncryptedStream)
@@ -46,33 +26,56 @@ namespace KS.Fiks.Crypto
             encryptionStream.Seek(0L, SeekOrigin.Begin);
             return encryptionStream;
         }
+
+        public Stream CreateEncryptionStream()
+        {
+            var cmsEnvelopedDataStreamGenerator = new CmsEnvelopedDataStreamGenerator();
+            cmsEnvelopedDataStreamGenerator.AddKeyTransRecipient(this.certificate);
+            return new EncodedStream(new MemoryStream(), cmsEnvelopedDataStreamGenerator);
+        }
+
+        public static class Factory
+        {
+            public static BCCryptoService Create(X509Certificate certificate)
+            {
+                if (certificate == null) throw new ArgumentNullException(nameof(certificate));
+
+                return new BCCryptoService(certificate);
+            }
+        }
     }
 
     internal class EncodedStream : Stream
     {
-        private readonly Stream _backingStream;
-        private readonly Stream _cryptoStream;
+        private readonly Stream backingStream;
+        private readonly Stream cryptoStream;
 
 
         public EncodedStream(Stream backingStream, CmsEnvelopedDataStreamGenerator generator)
         {
-            _backingStream = backingStream;
-            _cryptoStream = generator.Open(_backingStream, CmsEnvelopedGenerator.Aes256Cbc);
+            this.backingStream = backingStream;
+            this.cryptoStream = generator.Open(this.backingStream, CmsEnvelopedGenerator.Aes256Cbc);
         }
+
+        public override bool CanRead => this.backingStream.CanRead;
+        public override bool CanSeek => this.backingStream.CanSeek;
+        public override bool CanWrite => this.cryptoStream.CanWrite;
+        public override long Length => this.backingStream.Length;
+        public override long Position { get; set; }
 
         public override void Flush()
         {
-            _cryptoStream.Flush();
+            this.cryptoStream.Flush();
         }
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            return _backingStream.Read(buffer, offset, count);
+            return this.backingStream.Read(buffer, offset, count);
         }
 
         public override long Seek(long offset, SeekOrigin origin)
         {
-            return _backingStream.Seek(offset, origin);
+            return this.backingStream.Seek(offset, origin);
         }
 
         public override void SetLength(long value)
@@ -82,19 +85,13 @@ namespace KS.Fiks.Crypto
 
         public override void Write(byte[] buffer, int offset, int count)
         {
-            _cryptoStream.Write(buffer, offset, count);
+            this.cryptoStream.Write(buffer, offset, count);
         }
-
-        public override bool CanRead => _backingStream.CanRead;
-        public override bool CanSeek => _backingStream.CanSeek;
-        public override bool CanWrite => _cryptoStream.CanWrite;
-        public override long Length => _backingStream.Length;
-        public override long Position { get; set; }
 
         protected override void Dispose(bool disposing)
         {
-            _cryptoStream.Dispose();
-            _backingStream.Dispose();
+            this.cryptoStream.Dispose();
+            this.backingStream.Dispose();
         }
     }
 }
