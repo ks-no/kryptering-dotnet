eb1d43e53c970e26510b948719b40be9134a4e25
Sondre Eikanger Kvalø <zapodot@gmail.com>
2019-03-20 12:58:11 +0100

    Add test for extracting RSA private key

--- KS.Fiks.Crypto.Tests/BouncyCastle/AsymmetricKeyParameterReaderTest.cs
+++ KS.Fiks.Crypto.Tests/BouncyCastle/AsymmetricKeyParameterReaderTest.cs
@@ -14,5 +14,14 @@ namespace KS.Fiks.Crypto.Tests.BouncyCastle
             asymmetricKeyParameter.Should().NotBeNull();
             asymmetricKeyParameter.IsPrivate.Should().BeTrue();
         }
+
+        [Fact(DisplayName = "Extract private RSA key")]
+        public void ExtractPrivateRSAKey()
+        {
+            var privateRsaKeyString = TestDataUtil.GetContentFromResource("rsa_private_key.pem");
+            var asymmetricKeyParameter = AsymmetricKeyParameterReader.ExtractPrivateKey(privateRsaKeyString);
+            asymmetricKeyParameter.Should().NotBeNull();
+            asymmetricKeyParameter.IsPrivate.Should().BeTrue();
+        }
     }
 }
