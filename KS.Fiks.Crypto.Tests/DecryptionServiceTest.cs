a38dd988267f67c2dfc9e746cbef18fcad55da8f
Andreas <andreas.lilleskare@dfind.no>
2023-05-30 13:24:16 +0200

    Support multiple private keys

--- KS.Fiks.Crypto.Tests/DecryptionServiceTest.cs
+++ KS.Fiks.Crypto.Tests/DecryptionServiceTest.cs
@@ -41,6 +41,25 @@ namespace KS.Fiks.Crypto.Tests
             }
         }
 
+        [Fact(DisplayName = "Decryption with multiple private keys")]
+        public void DecryptWithInvalidAndValidPrivateKey()
+        {
+            var unencryptedDataChunk = TestDataUtil.GetContentFromResource("UnencryptedData.txt");
+            var encryptedData = Encrypt(unencryptedDataChunk);
+            var cryptoService = DecryptionService.Create(new[]
+                {TestDataUtil.ReadInvalidPrivateKey(), TestDataUtil.ReadPrivateKey() });
+            using var encryptedDataStream = new MemoryStream(encryptedData);
+            using var decryptedDataStream = cryptoService.Decrypt(encryptedDataStream);
+            using var decryptedStuff = new MemoryStream();
+
+            decryptedDataStream.CopyTo(decryptedStuff);
+            var decryptedDataChunk = decryptedStuff.ToArray();
+            var decryptedDataString = Encoding.UTF8.GetString(decryptedDataChunk).TrimEnd();
+
+            decryptedDataString.Equals(unencryptedDataChunk, StringComparison.InvariantCulture)
+                .Should().BeTrue();
+        }
+
         private static byte[] Encrypt(string unencryptedData)
         {
             IEncryptionService encryptionService = EncryptionService.Create(TestDataUtil.ReadPublicCertificate());
--- KS.Fiks.Crypto/BouncyCastle/DecryptStreamConverter.cs
+++ KS.Fiks.Crypto/BouncyCastle/DecryptStreamConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Org.BouncyCastle.Cms;
@@ -10,12 +11,50 @@ namespace KS.Fiks.Crypto.BouncyCastle
     {
         public static Stream Decrypt(AsymmetricKeyParameter privateKey, Stream encryptedStream)
         {
+            if (privateKey == null)
+            {
+                throw new ArgumentNullException(nameof(privateKey));
+            }
+
+            var recipientInformation = GetRecipientInformation(encryptedStream);
+            return recipientInformation.GetContentStream(privateKey).ContentStream;
+        }
+
+        public static Stream Decrypt(IReadOnlyCollection<AsymmetricKeyParameter> privateKeys, Stream encryptedStream)
+        {
+            if (privateKeys == null)
+            {
+                throw new ArgumentNullException(nameof(privateKeys));
+            }
+
+            var recipientInformation = GetRecipientInformation(encryptedStream);
+
+            using (var keysIterator = privateKeys.GetEnumerator())
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cs = recipientInformation.GetContentStream(keysIterator.Current);
+                        return cs.ContentStream;
+                    }
+                    catch (Exception)
+                    {
+                        if (!keysIterator.MoveNext())
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static RecipientInformation GetRecipientInformation(Stream encryptedStream)
+        {
             if (encryptedStream == null)
             {
                 throw new ArgumentNullException(nameof(encryptedStream));
             }
-
-            var pr = privateKey ?? throw new ArgumentNullException(nameof(privateKey));
 
             var cmsEnvelopedDataParser = new CmsEnvelopedDataParser(new BufferedStream(encryptedStream, 1024 * 1024));
             var encryptionAlgorithm = cmsEnvelopedDataParser.EncryptionAlgOid;
@@ -31,10 +70,7 @@ namespace KS.Fiks.Crypto.BouncyCastle
             }
 
             var recipients = recipientInformationStore.GetRecipients().Cast<RecipientInformation>();
-            var recipientInformation = recipients.AsEnumerable().First(r => true);
-            var cs = recipientInformation.GetContentStream(pr);
-
-            return cs.ContentStream;
+            return recipients.First();
         }
     }
 }
